473c4152ccd6da196f61b5f405a832977e104876
FelixDikland <f.dikland@gmail.com>
2021-03-11 17:22:18 +0100

    blep

--- main_sudoku.py
+++ main_sudoku.py
@@ -2,7 +2,7 @@ from skimage import io, color, measure, morphology, filters
 from matplotlib import pyplot as plt
 from skimage.feature import local_binary_pattern
 import numpy as np
-
+import math
 
 def preprocess(image_path):
     img = io.imread(image_path)
@@ -53,30 +53,30 @@ def find_mid(image):
     return sum(x)//total, sum(y)//total, [sum(row) for idx, row in enumerate(transposed)], [sum(row) for idx, row in enumerate(image)]
 
 
-def find_edges(mid_x, mid_y, x, y, thresh, rim):
+def find_edges(mid_x, mid_y, x, y, thresh):
 
-    min_x_upp = min(x[mid_x:len(x)-rim])
+    min_x_upp = min(x[mid_x:])
     x_upp = [value-min_x_upp for value in x[mid_x:]]
     for idx, value in enumerate(x_upp):
         if value < thresh:
             x_edge_upp = idx+mid_x
             break
 
-    min_x_low = min(x[mid_x:rim:-1])
+    min_x_low = min(x[mid_x::-1])
     x_low = [value - min_x_low for value in x[mid_x::-1]]
     for idx, value in enumerate(x_low):
         if value < thresh:
             x_edge_low = mid_x-idx
             break
 
-    min_y_upp = min(y[mid_y:len(y)-rim])
+    min_y_upp = min(y[mid_y:])
     y_upp = [value-min_y_upp for value in y[mid_y:]]
     for idx, value in enumerate(y_upp):
         if value < thresh:
             y_edge_upp = idx+mid_y
             break
 
-    min_y_low = min(y[mid_y:rim:-1])
+    min_y_low = min(y[mid_y::-1])
     y_low = [value - min_y_low for value in y[mid_y::-1]]
     for idx, value in enumerate(y_low):
         if value < thresh:
@@ -95,14 +95,56 @@ def edge_to_corner(x_edge, y_edge):
     return corner_coords
 
 
-def crop_to_edge(image, x_edge, y_edge):
-    
+def crop_to_edge(image, x_edge, y_edge, mid_x, mid_y):
+    image = image[y_edge[1]: y_edge[0],x_edge[1]:x_edge[0]]
+    return image, mid_x - x_edge[1], mid_y - y_edge[1]
 
 
 def plot_corners(corner_coords):
     corner_ofset = [corner_coords[(idx+1)%4] for idx, _ in enumerate(corner_coords)]
     for corner1, corner2 in zip(corner_coords, corner_ofset):
         plt.plot([corner1[0], corner2[0]], [corner1[1], corner2[1]])
+
+
+def get_x_y_pairs(image):
+    _, colomns = np.shape(image)
+    flat = image.flatten()
+    x_y_pair = [[idx%colomns, idx//colomns]  for idx, value in enumerate(flat) if value == 1]
+    return x_y_pair
+
+
+def get_euclidean_distances(x_y_pairs, mid_x, mid_y):
+    distances = [ math.sqrt((value[0] - mid_x)**2 + (value[1] - mid_y)**2) for value in x_y_pairs]
+    return distances
+
+
+def get_corners(x_y_pairs, distances, mid_x, mid_y):
+
+    dist_upp = 20
+    dist_low = 20
+    dist_left = 20
+    dist_right = 20
+
+    for coord, dist in zip(x_y_pairs, distances):
+        x = coord[0] - mid_x
+        y = coord[1] - mid_y
+        if x < y:
+            if (x < -1*y) and (dist > dist_left):
+                corn_left = coord
+                dist_left = dist
+            elif (x > -1*y) and (dist > dist_upp):
+                corn_upp = coord
+                dist_upp = dist
+        elif x > y:
+            if (x > -1*y) and (dist > dist_right):
+                corn_right = coord
+                dist_right = dist
+            if (x < -1*y) and (dist > dist_low):
+                corn_low = coord
+                dist_low = dist
+    
+    return [corn_left, corn_right, corn_upp, corn_low]
+
 
 
 image_paths =  ['sudoku_straight.jpeg', 'sudoku_diag.jpeg',  'sudoku_straight_tilted.jpeg', 'sudoku_diag_tilted.jpeg']
@@ -112,11 +154,20 @@ for idx, path in enumerate(image_paths):
 
     dark = thresh(image, 0.6)
     edges = edges_binar(dark)
+
     mid_x, mid_y, x, y = find_mid(edges)
     x_edge, y_edge = find_edges(mid_x, mid_y, x, y,2)
-    corners = edge_to_corner(x_edge, y_edge)
+
+    edges, mid_x, mid_y = crop_to_edge(edges, x_edge, y_edge, mid_x, mid_y)
+
+    x_y_pairs = get_x_y_pairs(edges)
+    distances = get_euclidean_distances(x_y_pairs, mid_x, mid_y)
+
+    corners = get_corners(x_y_pairs, distances,mid_x, mid_y)
+
     plt.subplot(2,3,(idx+1))
     plt.imshow(edges)
-    plot_corners(corners)
+    for corner in corners:
+        plt.plot(corner[0], corner[1], 'ro')
 
 io.show()
